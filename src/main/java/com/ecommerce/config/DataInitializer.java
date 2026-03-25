af1043249ea7bba3b82caa38f08a0b66386dfeeb
Lingesh <lingesh7695@gmail.com>
2026-03-25 13:32:21 +0530

    Update DataInitializer.java

--- src/main/java/com/ecommerce/config/DataInitializer.java
+++ src/main/java/com/ecommerce/config/DataInitializer.java
@@ -1,4 +1,5 @@
-package com.ecommerce.config;
+ package com.ecommerce.config;
+
 import com.ecommerce.entity.User;
 import com.ecommerce.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -6,14 +7,18 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
 @Configuration
 public class DataInitializer {
-@Autowired
+
+    @Autowired
     private UserRepository userRepository;
+
     @Autowired
     private PasswordEncoder passwordEncoder;
-   @Bean
-     public CommandLineRunner initDatabase() {
+
+    @Bean
+    public CommandLineRunner initDatabase() {
         return args -> {
             User existingUser = userRepository.findByUsername("lingesh");
             if (existingUser == null) {
